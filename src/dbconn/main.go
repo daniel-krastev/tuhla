d14fcecc4c212b1ffa73430519f592e65a145659
Daniel Krastev <danielkrastev@yahoo.com>
2023-02-07 22:31:15 +0000

    example DB connection

--- src/dbconn/main.go
+++ src/dbconn/main.go
@@ -1,7 +1,31 @@
 package main
 
 import (
-	_ "github.com/jackc/pgx/v5"
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/jackc/pgx/v5"
 )
 
-func main() {}
+const connString = "postgresql://root@localhost:26257/defaultdb?sslmode=disable"
+
+func main() {
+	ctx := context.Background()
+	conn, err := pgx.Connect(ctx, connString)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+		os.Exit(1)
+	}
+	defer conn.Close(ctx)
+
+	conn.Exec(ctx, "USE tuhla")
+	var name, address string
+	err = conn.QueryRow(context.Background(), "select name, address from app.houses where id=$1", 3).Scan(&name, &address)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Println(name, address)
+}
